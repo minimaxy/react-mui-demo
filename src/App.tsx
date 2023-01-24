54ef582674ca6bc8d5add11035977370338fdc6c
Minim <minimarisuto@gmail.com>
2023-01-24 11:02:48 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { MuiLayout } from "./components/MuiLayout";
+// import { MuiAutocomplete } from "./components/MuiAutocomplete";
+// import { MuiRating } from "./components/MuiRating";
+// import { MuiSwitch } from './components/MuiSwitch';
+// import { MuiCheckbox } from './components/MuiCheckbox';
+// import { MuiRadioButton } from './components/MuiRadioButton';
+// import { MuiSelect } from './components/MuiSelect';
+// import { MuiButton } from './components/MuiButton';
+// import { MuiTextField } from './components/MuiTextField';
+// import { MuiTypography } from './components/MuiTypography';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MuiTypography /> */}
+      {/* <MuiButton /> */}
+      {/* <MuiTextField/> */}
+      {/* <MuiSelect /> */}
+      {/* <MuiRadioButton /> */}
+      {/* <MuiCheckbox /> */}
+      {/* <MuiSwitch /> */}
+      {/* <MuiRating /> */}
+      {/* <MuiAutocomplete /> */}
+      <MuiLayout />
+    </div>
+  );
+}
+
+export default App;
